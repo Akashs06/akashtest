65213f952e660c637195c72fe3a2c2737748df00
Akashs06 <akashskrish@gmail.com>
2023-01-20 14:01:06 +0530

    first

---
+++ src/main/java/Akashtest/Akashtest/App.java
@@ -0,0 +1,22 @@
+package Akashtest.Akashtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args)
+
+	{
+
+		
+		System.out.println("third message");
+		System.out.println("for try");
+		
+		
+		
+		System.out.println("hello");
+		System.out.println("Hello World!");
+	}
+}
